2d21266aa48f396a28fe4d0957eb1d31d2982756
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-06-07 17:43:15 +0700

    10. Forms

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 
-use Illuminate\Http\Request;
+
 use App\Article;
 use App\Http\Requests;
-
+use Request;
+use Carbon\Carbon;
 class ArticlesController extends Controller
 {
     public function index(){
-      $articles = Article::all();
+      /* $articles = Article::all(); */ //make articles down to the bottom, expected top
+      $articles = Article::latest('published_at')->get();
       return view('articles.index', compact('articles'));
     }
 
@@ -22,5 +24,22 @@ class ArticlesController extends Controller
         abort(404);
       }*/
       return view('articles.show', compact('article'));
+    }
+
+    public function create(){
+      return view('articles.create');
+    }
+
+    public function store(){
+      $input = Request::all();
+
+      $input['published_at'] = Carbon::now();
+      Article::create($input);
+      //same as
+      /*
+      $article = new Article;
+      $article->title = $input['title'];
+      */
+      return redirect('articles');
     }
 }
